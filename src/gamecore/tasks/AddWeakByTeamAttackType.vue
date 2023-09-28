bfe6bf85ba4a4d790c5a3a77a96e154a7231df43
Damnae <damnaee@gmail.com>
2023-09-29 01:21:35 +0200

    Implement some tasks.

---
+++ src/gamecore/tasks/AddWeakByTeamAttackType.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  import { GamecoreTask, GamecoreTargetType, DynamicExpression, } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+  import EvaluateTargetType from '../EvaluateTargetType.vue';
+  import EvaluateExpression from '../EvaluateExpression.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    TargetType?:GamecoreTargetType
+    ResistanceDeltaValue?:DynamicExpression
+    AddWeakCountMax?:DynamicExpression
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+    
+    Add a weakness based on team elements 
+    <template v-if="node.TargetType">
+      to <em><EvaluateTargetType :target="node.TargetType" /></em>,
+    </template>
+    <template v-if="node.ResistanceDeltaValue">
+      modifying corresponding RES by <em><EvaluateExpression :expression="node.ResistanceDeltaValue" /></em>
+    </template>    
+    <template v-if="node.AddWeakCountMax">
+      with a max of <em><EvaluateExpression :expression="node.AddWeakCountMax" /></em> weaknesses
+    </template>
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
---
+++ src/gamecore/tasks/AddWeakness.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { GamecoreTask, GamecoreTargetType, } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+  import EvaluateTargetType from '../EvaluateTargetType.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    TargetType?:GamecoreTargetType
+    DamageType?:string
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+    
+    Add a <em>{{ node.DamageType }}</em> weakness
+    <template v-if="node.TargetType">
+      to <em><EvaluateTargetType :target="node.TargetType" /></em>,
+    </template>
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
