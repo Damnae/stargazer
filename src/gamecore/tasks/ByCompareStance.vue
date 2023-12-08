809cfc6bbdb52a0450266eb96f50420891a3ac8b
Damnae <damnaee@gmail.com>
2023-12-08 14:41:03 +0100

    Implement some tasks.

---
+++ src/gamecore/tasks/ByCompareStance.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { GamecoreTask, DynamicExpression, GamecoreTargetType, } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+  import EvaluateTargetType from '../EvaluateTargetType.vue';
+  import EvaluateExpression from '../EvaluateExpression.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    TargetType?:GamecoreTargetType
+    CompareType:string
+    CompareValue:DynamicExpression
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+    
+    <em><EvaluateTargetType :target="node.TargetType" /></em>
+    toughness is
+    <template v-if="node.CompareType">
+      <em>{{ node.CompareType }}</em> to 
+    </template>
+    <template v-if="node.CompareValue">
+      <em><EvaluateExpression :expression="node.CompareValue" /></em>
+    </template>
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
