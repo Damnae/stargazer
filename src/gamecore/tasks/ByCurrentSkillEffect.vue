7edc75abd8337b1d772c81e5ce0889ef7dee7224
Damnae <damnaee@gmail.com>
2023-12-09 16:12:20 +0100

    Implement some tasks.

---
+++ src/gamecore/tasks/ByCurrentSkillEffect.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { GamecoreTask, } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    SkillEffectList:string[]
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+
+    Current skill effect
+    is <em>{{ node.SkillEffectList.join(', ') }}</em>
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
---
+++ src/gamecore/tasks/ModifyTeamBoostPointMax.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { GamecoreTask,
+    GamecoreTargetType, 
+    DynamicExpression, 
+  } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+  import EvaluateExpression from '../EvaluateExpression.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    TargetType?:GamecoreTargetType
+    ModifyFunction:string
+    ModifyValue?:DynamicExpression
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+    
+    {{ node.ModifyFunction }} <em><EvaluateExpression :expression="node.ModifyValue" /></em> max skill point
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
