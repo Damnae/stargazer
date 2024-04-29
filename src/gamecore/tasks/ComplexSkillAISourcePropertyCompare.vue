0057c9394db249e580eb29e805e259faa5aa88d1
Damnae <damnaee@gmail.com>
2024-04-29 23:57:34 +0200

    Implement some tasks.

---
+++ src/gamecore/tasks/ComplexSkillAISourcePropertyCompare.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { GamecoreTask, } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    TargetPropertyType:string
+    CompareType:string
+    CompareValue:
+    {
+      Value:number
+    }
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+
+    {{ node.TargetPropertyType }} 
+    <template v-if="node.CompareType">
+      is <em>{{ node.CompareType }}</em> 
+      to <em>{{ node.CompareValue?.Value ?? 0 }}</em>
+    </template>
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
---
+++ src/gamecore/tasks/SetEntityActionState.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import { GamecoreTask, GamecoreTargetType, } from '@/sources/gamecore';
+  import BlockLayout from '@/components/BlockLayout.vue';
+  import EvaluateTargetType from '../EvaluateTargetType.vue';
+
+  const props = defineProps<{node:GamecoreTask}>()
+  const node = props.node as unknown as 
+  {
+    TargetType?:GamecoreTargetType
+    UniqueName?:string
+    ActionState?:boolean
+  }
+</script>
+
+<template>
+  <BlockLayout :source="node">
+    
+    {{ node.ActionState === false ? "Disable" : "Enable" }}
+    <template v-if="node.TargetType">
+      <em><EvaluateTargetType :target="node.TargetType" /></em>'s
+    </template>
+    action
+    <template v-if="node.UniqueName">
+      with name <em>{{ node.UniqueName }}</em>
+    </template>
+
+  </BlockLayout>
+</template>
+
+<style scoped>
+</style>
